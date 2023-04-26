b3cc739bc5e21527c238a8ccf287b08e231c8a2a
Sanjida Akter <sanjidakter2304@gmail.com>
2023-04-26 13:27:59 +0600

    news details showing

---
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -0,0 +1,70 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
+
+const NewsCard = ({ news }) => {
+  const { _id, title, details, image_url, author, rating, total_view } = news;
+  return (
+    <Card className=" mb-4">
+      <Card.Header className="d-flex align-items-center">
+        <img
+          style={{ height: "40px", borderRadius: "50%" }}
+          src={author?.img}
+        />
+        <div className="ps-2 flex-grow-1">
+          <p className="mb-0">{author?.name}</p>
+          <p>
+            <small>{moment(author?.published_date).format("yyyy-MM-D")}</small>
+          </p>
+        </div>
+        <div>
+          <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
+        </div>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Text>
+          {details.length < 250 ? (
+            <>
+              {}
+              {details}
+            </>
+          ) : (
+            <>
+              {details.slice(0, 250)}...{" "}
+              <Link to={`/news/${_id}`}>Read More</Link>
+            </>
+          )}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted d-flex">
+        <div className="flex-grow-1">
+          <Rating
+            placeholderRating={rating.number}
+            readonly
+            emptySymbol={
+              <FaRegStar></FaRegStar>
+            }
+            placeholderSymbol={
+              <FaStar className="text-warning"></FaStar>
+            }
+            fullSymbol={
+              <FaStar></FaStar>
+            }
+          />
+          {rating?.number}
+        </div>
+        <div>
+          <FaEye></FaEye> {total_view}
+        </div>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default NewsCard;
